6798d218d44f3eb23c0de629dcb80baf9290c206
amt-richard-dadzie <r.dadzie@amalitech.com>
2025-04-02 09:37:19 +0000

    refactor(resource): implement Resource function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  effect,
+  resource,
+  ResourceStatus,
+  signal,
+} from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FormsModule } from '@angular/forms';
+import { API_URL, User } from './app.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [MatProgressBarModule, FormsModule],
+  template: ` <div>
+      <label>Users Search</label>
+      <input placeholder="Search user by name" [(ngModel)]="query" />
+    </div>
+
+    @if(users.isLoading()){
+    <mat-progress-bar mode="query" />
+    } @if (users.status() === status.Error) {
+    <span class="error">{{ users.error() }}</span>
+    <button>Retry</button>
+    }
+
+    <section class="actions">
+      <button>Add User</button>
+      <button>Clear</button>
+    </section>
+
+    <ul>
+      @for (user of users.value(); track user.id) {
+      <li>{{ user.name }}</li>
+      }@empty {
+      <li class="no-data">Nothing to show</li>
+      }
+    </ul>`,
 })
 export class AppComponent {
-  title = 'angular-resource-demo';
+  query = signal('');
+  status = ResourceStatus;
+  users = resource<User[], { query: string }>({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request, abortSignal }) => {
+      const response = await fetch(`${API_URL}?name_like=^${request.query}`, {
+        signal: abortSignal,
+      });
+      if (!response.ok) {
+        throw new Error('Network error');
+      }
+      return await response.json();
+    },
+  });
+
+  constructor() {
+    effect(() => {
+      console.log('Status:', this.users.status());
+      console.log('Query:', this.query());
+    });
+  }
 }
